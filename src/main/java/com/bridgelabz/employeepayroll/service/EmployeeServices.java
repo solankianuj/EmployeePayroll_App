027db0491e7c06154f0e37d7527257242dad0e1f
solanki <anujaj2222.gmail.com>
2022-08-12 00:10:31 +0530

    Store Employee Data In A List And Performing CRUD Operation On Employee Payroll Data

--- src/main/java/com/bridgelabz/employeepayroll/service/EmployeeServices.java
+++ src/main/java/com/bridgelabz/employeepayroll/service/EmployeeServices.java
@@ -1,57 +1,48 @@
 package com.bridgelabz.employeepayroll.service;
-
 import com.bridgelabz.employeepayroll.employeeDTO.EmployeeDTO;
 import com.bridgelabz.employeepayroll.employeeModel.EmployeeModel;
 import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Service
 
 public class EmployeeServices  implements  IEmployeeServices{
-    @Override
-    public EmployeeModel getEmployeeData(EmployeeDTO employeeDTO) {
 
-        employeeDTO.setFname("Anuj");
-        employeeDTO.setLname("Solanki");
-        employeeDTO.setCompanyName("HP");
-        employeeDTO.setSalary(10000l);
-        EmployeeModel employeeModel =new EmployeeModel(1, employeeDTO);
-        return employeeModel;
+    List<EmployeeModel> employeeModelList=new ArrayList<>();
+    @Override
+    public List<EmployeeModel> getEmployeeData() {
+        return employeeModelList;
     }
 
     @Override
     public EmployeeModel getEmployee(int id) {
-        EmployeeDTO employeeDTO=new EmployeeDTO();
-        employeeDTO.setFname("Anuj");
-        employeeDTO.setLname("Solanki");
-        employeeDTO.setCompanyName("HP");
-        employeeDTO.setSalary(10000l);
-        EmployeeModel employeeModel =new EmployeeModel( id, employeeDTO);
-
-        return employeeModel;
+        return employeeModelList.get(id-1);
     }
-
-    @Override
-    public EmployeeModel addEmployee(EmployeeDTO employeeDTO) {
-        EmployeeModel employeeModel =new EmployeeModel( 1, employeeDTO);
-
-        return employeeModel;
-    }
+        @Override
+        public EmployeeModel addEmployee(EmployeeDTO employeeDTO) {
+            EmployeeModel employeeModel= new EmployeeModel(employeeModelList.size()+1,employeeDTO);
+               employeeModelList.add(employeeModel);
+            return employeeModel;
+        }
 
     @Override
     public EmployeeModel updateEmployee(int id,EmployeeDTO employeeDTO) {
-        EmployeeModel employeeModel =new EmployeeModel( id, employeeDTO);
+        EmployeeModel employeeModel =this.getEmployee(id);
+        employeeModel.setFname(employeeDTO.getFname());
+        employeeModel.setLname(employeeDTO.getLname());
+        employeeModel.setCompanyName(employeeDTO.getCompanyName());
+        employeeModel.setSalary(employeeDTO.getSalary());
+        employeeModelList.set(id-1,employeeModel);
 
         return employeeModel;
     }
 
     @Override
-    public EmployeeModel deleteEmployee(int id) {
-        EmployeeDTO employeeDTO=new EmployeeDTO();
-        employeeDTO.setFname("Anuj");
-        employeeDTO.setLname("Solanki");
-        employeeDTO.setCompanyName("HP");
-        employeeDTO.setSalary(10000l);
-        EmployeeModel employeeModel =new EmployeeModel( id, employeeDTO);
-        return employeeModel;
+    public void deleteEmployee(int id) {
+
+        employeeModelList.remove(id-1);
+
     }
 }
--- src/main/java/com/bridgelabz/employeepayroll/service/IEmployeeServices.java
+++ src/main/java/com/bridgelabz/employeepayroll/service/IEmployeeServices.java
@@ -3,11 +3,13 @@ package com.bridgelabz.employeepayroll.service;
 import com.bridgelabz.employeepayroll.employeeDTO.EmployeeDTO;
 import com.bridgelabz.employeepayroll.employeeModel.EmployeeModel;
 
+import java.util.List;
+
 public interface IEmployeeServices {
 
-    EmployeeModel getEmployeeData(EmployeeDTO employeeDTO);
+    List<EmployeeModel> getEmployeeData();
     EmployeeModel getEmployee(int id);
     EmployeeModel addEmployee(EmployeeDTO employeeDTO);
     EmployeeModel updateEmployee(int id ,EmployeeDTO employeeDTO);
-    EmployeeModel deleteEmployee(int id);
+    void deleteEmployee(int id);
 }
